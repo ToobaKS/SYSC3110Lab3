327d43ab270572e0c47f5ccec9f9d2f7c1a2c3ee
ToobaKS <tksheikh17@gmail.com>
2021-09-17 12:55:30 -0400

    Added/Removed buddy

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -4,8 +4,8 @@ public class AddressBook {
 
     private ArrayList<BuddyInfo> buddies;
 
-    public AddressBook(ArrayList<BuddyInfo> buddy) {
-        this.buddies = buddy;
+    public AddressBook() {
+        buddies = new ArrayList<>();
     }
 
     public void removeBuddy(BuddyInfo buddy) {
@@ -17,7 +17,11 @@ public class AddressBook {
     }
 
     public static void main(String[] args) {
-        //BuddyInfo buddy = new BuddyInfo("Tooba","Canada","1");
+        BuddyInfo buddy = new BuddyInfo("Tooba","Canada","1");
+        AddressBook buddies = new AddressBook();
+
+        buddies.addBuddy(buddy);
+        buddies.removeBuddy(buddy);
 
         System.out.println("Address Book");
     }
