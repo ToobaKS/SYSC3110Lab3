dfe51f7a16e4d6f0356c8c3cf7e7d78984f6de35
ToobaKS <tksheikh17@gmail.com>
2021-09-24 12:29:35 -0400

    Buddy Info

---
+++ src/BuddyInfo.java
@@ -0,0 +1,30 @@
+public class BuddyInfo {
+
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+    public BuddyInfo(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public static void main(String[] args) {
+        BuddyInfo buddy = new BuddyInfo("Tooba","Canada","1");
+
+        System.out.println("Hello " + buddy.getName());
+    }
+}
